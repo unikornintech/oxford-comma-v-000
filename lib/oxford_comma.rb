5d48aa0c13ed37f02e5a191338e1bf88382de83c
Lauren Days <lmchambers0420@gmail.com>
2018-11-23 05:53:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,4 +12,12 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-  
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_element = array.pop
+    array.join(", ") + ", and " + last_element
+  end
+end
