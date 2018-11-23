d50d2ff069a0782576fa66dea1f905ecb95bac6f
Lauren Days <lmchambers0420@gmail.com>
2018-11-23 06:05:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  array.join
+end
 
+def oxford_comma(array)
+  array.join(" and ")
+end
+
+def oxford_comma(array)
+  return array.join if array.nil? or array.length <= 3
+  array.join(", ") + " and " + array.last
+end
+
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    last_element = array.pop
+    array.join(", ") + ", and " + last_element
+  end
 end
